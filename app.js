b25f8bbb4faa8d942a1e917e0c3dbb4bf9d45e98
niki2422 <randazzonico55@gmail.com>
2024-07-14 19:21:34 -0300

    se crearon archivos .ejs, se les dio estilo, y se crearon las paginas login y register

--- app.js
+++ app.js
@@ -4,17 +4,27 @@ const path = require ('path');
 const app = express();
 const PORT = 3000;
 
+// Configurar el motor de vistas
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 // Servir archivos estáticos desde el directorio 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta para la carta de producto
-app.get('/producto', (req, res) => {
-  res.sendFile(path.join(__dirname, '.views/cart-product.html'));
+// Ruta para login
+app.get('/login', (req, res) => {
+  res.render('users/login.ejs');
 });
+
+// Ruta para registro
+app.get('/registro', (req, res) => {
+  res.render('users/register.ejs');
+});
+
 
 // Ruta explícita para la raíz "/"
 app.get('/', (req, res) => {
-  res.render(path.join(__dirname, './views/books/index.ejs'));
+  res.render('books/index.ejs');
 });
 
 
